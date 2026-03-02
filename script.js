25ff22a9e7bb89adc31f6842b3152b03cb925633
Venktesh Nandan <venkteshnandan09@gmail.com>
2026-03-02 11:06:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggle = document.getElementById("theme-toggle");
+
+toggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    toggle.textContent =
+        document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
